a13e1dd7d64c7b27ccc1c3367f1c32fc26ea5ae7
[dyad] <nicolasmorais154@gmail.com>
2025-12-03 08:57:24 -0300

    [dyad] Definindo Merriweather como fonte padrão - wrote 1 file(s)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${merriweather.variable} font-sans antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${merriweather.variable} font-merriweather antialiased`}
       >
         {children}
       </body>
